9502828a53cb5e80a7a5b204cb16890dcd08694c
Kody <kihnat@gmail.com>
2018-04-07 14:59:52 -0500

    Reading Data!

--- models/Book.js
+++ models/Book.js
@@ -5,7 +5,18 @@ let db = new sqlite3.Database('../DB/healthHack.db', (err) => {
   if (err) {
     console.error(err.message);
   }
-  console.log('Connected to the chinook database.');
+  console.log('Connected to the Health Database database.');
+});
+
+db.serialize(() => {
+  db.each(`SELECT Id as id,
+                  Name as name
+           FROM Modules`, (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(row.id + "\t" + row.name);
+  });
 });
  
 // close the database connection
