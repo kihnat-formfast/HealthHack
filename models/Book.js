b3521ccb62650b718c17124e238186bf140b7667
zubathorde <38145685+zubathorde@users.noreply.github.com>
2018-04-07 15:26:35 -0500

    this is a mess

--- models/Book.js
+++ models/Book.js
@@ -1,17 +1,13 @@
-const sqlite3 = require('sqlite3').verbose();
- 
-// open database in file system
-let db = new sqlite3.Database('../DB/healthHack.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the chinook database.');
+var mongoose = require('mongoose');
+
+var BookSchema = new mongoose.Schema({
+  isbn: String,
+  title: String,
+  author: String,
+  description: String,
+  published_year: String,
+  publisher: String,
+  updated_date: { type: Date, default: Date.now },
 });
- 
-// close the database connection
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+
+module.exports = mongoose.model('Book', BookSchema);
